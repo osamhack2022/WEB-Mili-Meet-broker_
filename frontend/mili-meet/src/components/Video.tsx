975caef004fa5ac2ab3f74a15d21d02305066551
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-09-27 09:45:41 +0000

    [feature] 싱글 페이지에서 webRTC 통신 예제 구현

--- frontend/mili-meet/src/components/Video.tsx
+++ frontend/mili-meet/src/components/Video.tsx
@@ -1,24 +1,24 @@
 import { styled } from '@mui/material';
-import { useEffect, useRef } from 'react';
+import { RefObject, useEffect } from 'react';
 
 interface VideoProps {
   mediaStream: MediaStream | undefined;
+  test: RefObject<HTMLVideoElement> | undefined;
 }
 
 const VideoStyled = styled('video')({
   backgroundColor: 'black'
 });
 
-function Video({ mediaStream }: VideoProps) {
-  const videoRef = useRef<HTMLVideoElement>(null);
+function Video({ mediaStream, test }: VideoProps) {
 
   useEffect(() => {
-    if (!mediaStream || !videoRef.current) return;
-    videoRef.current.srcObject = mediaStream;
-  }, [mediaStream, videoRef]);
+    if (mediaStream === undefined || test === undefined || test.current === null) return;
+    test.current.srcObject = mediaStream;
+  }, [mediaStream, test]);
 
   return (
-    <VideoStyled ref={videoRef} width="640" height="360" autoPlay></VideoStyled>
+    <VideoStyled ref={test} width="640" height="360" autoPlay></VideoStyled>
   );
 }
 
--- frontend/mili-meet/src/pages/caller.tsx
+++ frontend/mili-meet/src/pages/caller.tsx
@@ -2,6 +2,43 @@ import { Button } from '@mui/material';
 import { useState } from 'react';
 import Video from '../components/Video';
 
+// const RTC_CONFIGURATION = {
+//   iceServers: [
+//     { urls: 'stun:stun.l.google.com:19302' },
+//     {
+//       urls: 'turn:numb.viagenie.ca',
+//       credential: 'muazkh',
+//       username: 'webrtc@live.com'
+//     }
+//   ]
+// };
+
+// async function makePeerConnection(mediaStream: MediaStream) {
+//   let pc = new RTCPeerConnection(RTC_CONFIGURATION);
+//   mediaStream.getTracks().forEach((track) => {
+//     pc.addTrack(track);
+//   });
+
+//   pc.onicecandidate = (event) => {
+//     if (event.candidate != null) {
+//       // socket.emit('candidate', { candidate: event.candidate });
+//       console.log('Candidate Sent:', { candidate: event.candidate });
+//     }
+//   };
+
+//   pc.oniceconnectionstatechange = () => {
+//     if (pc.iceConnectionState === "disconnected") {
+//       pc.close();
+//       console.log("PC CLOSED!!!@#!@#!@#!");
+//     }
+//   };
+
+//   const offer = await pc.createOffer();
+//   await pc.setLocalDescription(offer);
+//   console.log('offer:', pc.localDescription);
+//   // socket.emit('offer', { offer: pc.localDescription });
+// }
+
 function Caller() {
   const [mediaStream, setMediaStream] = useState<MediaStream>();
 
