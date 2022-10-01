c7499c4a036a13eb68083b27333207757d830b4c
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-10-01 09:53:58 +0000

    [feature] webRTC proto socket 서버 구현

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import { Server } from 'socket.io';
+
+const io = new Server(3000);
+
+io.on("connection", (socket) => {
+  socket.on('offer', (offer) => {
+    io.emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    io.emit('answer', answer);
+  });
+
+  socket.on('caller-icecandidate', (icecandidate) => {
+    io.emit('caller-icecandidate', icecandidate);
+  });
+
+  socket.on('callee-icecandidate', (icecandidate) => {
+    io.emit('callee-icecandidate', icecandidate);
+  });
+});
